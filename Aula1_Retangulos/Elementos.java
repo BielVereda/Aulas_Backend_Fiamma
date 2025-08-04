d68a92e1c46980b8856784ed4263e8d01776dfe2
Gabriel Vereda <gabrielsantosvereda@gmail.com>
2025-08-04 08:56:26 -0300

    Esse é o código que contém os elementos do construtor do Main.

---
+++ Aula1_Retangulos/Elementos.java
@@ -0,0 +1,26 @@
+package Aula1_Retangulos;
+
+public class Elementos {
+	
+	public double lado;
+	public double altura;
+	
+	public double lado2;
+	public double altura2;
+	
+	public double lado3;
+	public double altura3;
+	
+	public double calculoArea() {
+		return altura*lado;
+	}
+	
+	public double calculoArea2() {
+		return altura2*lado2;
+	}
+	
+	public double calculoArea3() {
+		return altura3*lado3;
+	}
+
+}
