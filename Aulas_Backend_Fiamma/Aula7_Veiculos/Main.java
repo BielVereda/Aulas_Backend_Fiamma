2e4b9c296589a9c1aa4dcc06dca2cba5d9e1c158
Gabriel dos Santos Vereda <165197600+BielVereda@users.noreply.github.com>
2025-08-20 10:27:40 -0300

    Create Main.java

---
+++ Aulas_Backend_Fiamma/Aula7_Veiculos/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Veiculo v = new Veiculo();
+        v.iniciar();  // imprime "Veículo está dando partida"
+
+        Carro c = new Carro();
+        c.iniciar();  // imprime "Carro está dando partida"
+    }
+}
