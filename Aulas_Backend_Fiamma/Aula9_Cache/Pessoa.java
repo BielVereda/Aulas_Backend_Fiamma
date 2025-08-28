f0c45216459d50b1f259580d07992f9903b2abb8
Gabriel dos Santos Vereda <165197600+BielVereda@users.noreply.github.com>
2025-08-28 08:22:05 -0300

    Update Pessoa.java

--- Aulas_Backend_Fiamma/Aula9_Cache/Pessoa.java
+++ Aulas_Backend_Fiamma/Aula9_Cache/Pessoa.java
@@ -9,17 +9,36 @@ public class Pessoa {
         this.idade = idade;
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public int getId() {
+        return id;
+    }
 
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
+    public String getNome() {
+        return nome;
+    }
 
-    public int getIdade() { return idade; }
-    public void setIdade(int idade) { this.idade = idade; }
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 
     @Override
     public String toString() {
-        return "Pessoa{id=" + id + ", nome='" + nome + "', idade=" + idade + "}";
+        return "Pessoa{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", idade=" + idade +
+                '}';
     }
 }
