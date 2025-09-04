c138124623a67d93ad1aed956eef76bcc75c0860
Gabriel dos Santos Vereda <165197600+BielVereda@users.noreply.github.com>
2025-09-04 09:07:01 -0300

    Create ContaPoupanca.java

---
+++ Aulas_Backend_Fiamma/Aula10_Banco/ContaPoupanca.java
@@ -0,0 +1,11 @@
+public class ContaPoupanca extends Conta {
+
+    public ContaPoupanca(String numero, String agencia, String titular, double saldoInicial) {
+        super(numero, agencia, titular, saldoInicial);
+    }
+
+    @Override
+    public double calcularTarifaMensal() {
+        return 0.00;
+    }
+}
